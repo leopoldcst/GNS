a54de9d596a87a822ef532f2b14cff29ac046ed1
leopold_cst <leopoldconstant15@gmail.com>
2026-01-07 15:54:23 +0100

    ajout de ospfconfig et ripconfig bien propre !

--- commands.py
+++ commands.py
@@ -4,20 +4,73 @@ def baseRouterConfig(name):
         "configure terminal",
         f"hostname {name}",
         "ipv6 unicast-routing",
-        "end"
+        "end",
+        " "
     ]
 
-def addressConfig(interface,address): ## pas sûr à revoir 
+def addressConfig(interface,address):
+
     return [
         "enable",
         "configure terminal",
         f"interface {interface}",
-        " ipv6 enable",
-        f" ipv6 address {address}",
-        " no shutdown",
-        " exit",
-        "end"
+        "ipv6 enable",
+        f"ipv6 address {address}",
+        "no shutdown",
+        "exit",
+        "end",
+        " " # on force le end à se faire
     
     ]
+
+
+def ripConfig(address, interface, name):
+
+    process_name = "RIP_AS"
+    conf = []
+    conf += baseRouterConfig(name)
+    conf += addressConfig(interface,address)
+    conf += [
+        "enable"
+        "configure terminal"
+        f"interface {interface}",
+        f"ipv6 router rip {process_name}",
+        "exit",
+        f"interface {interface}",
+        f"ipv6 rip {process_name} enable",
+        "end",
+        " "
+    ]
+
+    return conf
+
+def ospfConfig(address, interface, name, area_nb):
+    conf = []
+    process_id = int(name[1:])
+    conf += baseRouterConfig(name)
+    conf += addressConfig(interface,address)
+    conf += [
+        "enable",
+        "configure terminal",
+        f"ipv6 router ospf {process_id}",
+        f"router-id {process_id}.{process_id}.{process_id}.{process_id}",
+        f"interface {interface}"
+        f"ipv6 ospf {process_id} area {area_nb}",
+        "end",
+        " "
+    ]
+
+    return conf
+
+
+    
+    
+    
+    
+
+   #"redistribute connected" à activer si on veut partager tous les sous reseaux auxquels on appartient, ce qui n'est pas le cas tout le temps :)
+
+
+ripConfig("1001:22:22", "g1/0", "R1")
 
 # il faut reprendre OSPF, RIP, BGP ici en simplifiant toutes les fonctions et en utilisant celles du dessus ! 
