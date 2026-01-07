39be7f14ffa85da68eb03b7f37c34f7d7a100873
hugonivert <hugo.nivert@gmail.com>
2026-01-07 08:57:37 +0100

    review ospf commandes

--- commandes_ospf.py
+++ commandes_ospf.py
@@ -6,19 +6,18 @@ Created on Tue Dec 16 16:36:39 2025
 """
 import json
 
-FICHIER_JSON = "config.json"
-ROUTEUR_CIBLE = "R3"   # <- change ici si besoin
+FICHIER_JSON = "config_final_struct.json"
+
 
 def ospf_commandes(addresse_ipv6,interface,nom_routeur):
     # Charger le JSON
     with open(FICHIER_JSON, "r", encoding="utf-8") as f:
         data = json.load(f)
 
-    as_data = data[0]
-
+    ROUTEUR_CIBLE = "R3"   # <- change ici si besoin avec une fonction qui creer les liens demandés
     # Trouver le routeur cible
     routeur = None
-    for r in as_data["routeurs"]:
+    for r in data["routeurs"]:
         if r["nom"] == ROUTEUR_CIBLE:
             routeur = r
             break
@@ -29,7 +28,11 @@ def ospf_commandes(addresse_ipv6,interface,nom_routeur):
 
     commandes = []
     process_id = int(nom_routeur[1:])
-    area_nb = 1
+    for ro in data["routeurs"]:
+        if ro["nom"] == nom_routeur:
+            area_nb = ro["as"]
+            break
+    
 
     
     # Début configuration
@@ -55,17 +58,28 @@ def ospf_commandes(addresse_ipv6,interface,nom_routeur):
     # Affichage (ou écriture fichier)
     for cmd in commandes:
         print(cmd)
+
+       '''
+       fin programme OSPF
+       début programme RIP 
+       ''' 
         
 def rip_commandes(addresse_ipv6,interface, nom_routeur):
     # Charger le JSON
     with open(FICHIER_JSON, "r", encoding="utf-8") as f:
         data = json.load(f)
 
-    as_data = data[0]
-
+  
+    ROUTEUR_CIBLE = "R3"   # <- change ici si besoin avec une fonction qui creer les liens demandés
+    #exemple :
+    #for r in data["liens"]:
+    #    if r["from"] == nom_routeur:
+    #        ROUTEUR_CIBLE = r["to"]
+    #        break
+    
     # Trouver le routeur cible
     routeur = None
-    for r in as_data["routeurs"]:
+    for r in data["routeurs"]:
         if r["nom"] == ROUTEUR_CIBLE:
             routeur = r
             break
