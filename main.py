104907d34c99ff9fb841ccc6272535f4ed28477e
alietar <ajm.lietar@gmail.com>
2026-01-07 17:03:06 +0100

    Refactor et correction de bugs

--- main.py
+++ main.py
@@ -1,84 +1,83 @@
 import json
 import gns
 import commands
+from ip_utils import *
 
-FICHIER_JSON = "config_final_struct.json"
+INTENT_PATH = "intent_projet.json"
+
+def find_as(intent, name):
+    for router in intent["routers"]:
+        if router["name"] == name:
+            return router["as"]
+
+
+def find_internal_protocol(intent, as_nb):
+    for as_ in intent["as"]:
+        if as_["nb"] == as_nb:
+            return as_["internal_protocol"]
+
+
+def configure_interface(intent, name, interface, to):
+        as_nb = find_as(intent, name)
+        addr = ipv6_link_intra_as(name, to, as_nb)[name]
+
+        print(f"Upping interface {interface} on {name}")
+
+        g.run_on_router(name, commands.address_config(interface, addr)) # Up the interface and setting ip address
+        
+        if link["type"] == "intra-as":
+            if find_internal_protocol(intent, as_nb) == "RIP":
+                print(f"Enabling RIP at {interface} on {name}")
+                # g.run_on_router(name, commands.rip_config(link["from"]), addr)
+
+            elif find_internal_protocol(intent, as_nb) == "OPSF":
+                print(f"Enabling OSPF at {interface} on {name}")
+                # g.run_on_router(name, commands.opsf_config(link["from"]), addr)
+
+        if link["type"] == "inter-as":
+            # Implement BGP
+            pass
+
 
 if __name__ == "__main__":
-    # Reading the intent file
-    f = open(FICHIER_JSON, "r", encoding="utf-8")
+    ### Reading the intent file
+    f = open(INTENT_PATH, "r", encoding="utf-8")
     intent = json.load(f)
     f.close()
 
 
+    ### Project opening
+    print("Opening the project...")
     g = gns.GnsProject(name=intent["project_name"])
-
-    g.createNew(autoRecover=True)
+    g.create_new(auto_recover=True)
     g.open()
-
     print("Opened the project")
 
+
+    ### Router setup
     for router in intent["routers"]:
         name = router["name"]
 
         print(f"Creating router {name}")
-        g.createRouter(name=name, autoRecover=True)
+        g.create_router(name=name, auto_recover=True)
+        g.routers[name].start()
 
         print("Configuring the router")
-        routerSocket = gns.RouterSocket(port=g.getRouterPort(name))
-        for cmd in commands.baseRouterConfig(name):
-            routerSocket.run(cmd)
+        g.run_on_router(name, commands.base_router_config(name))
+        
 
-        routerSocket.close()
+    ### Link and protocol setup
+    for link in intent["links"]:
+        print(f"Adding link from {link["interface_from"]} on {link["from"]} to {link["interface_to"]} on {link["to"]}")
+        g.create_link(link["from"], # Adding link inside GNS
+                    link["interface_from"],
+                    link["to"],
+                    link["interface_to"])
 
-    #for link in intent["links"]:
-    #    print(f"Adding link from {link["from"]} to {link["to"]}")
-    #    try:
-    #        g.createLink(link["from"],
-    #                    link["interface_from"],
-    #                    link["to"],
-    #                    link["interface_to"])
-    #    except:
-    #        print("Link already exists")
 
-    #    if link["type"] == "intra-as":
-    #        for router in intent["routers"]:
-    #            if router["name"] == link["from"]:
-    #                if link[""]
+        # Configure the interface for both routers of the link
+        configure_interface(intent, link["from"], link["interface_from"], link["to"])
+        configure_interface(intent, link["to"], link["interface_to"], link["from"])
 
-    #                rip_commandes("1000:0:0:1::1/64", link["from_interface"], link["from"], link["to"])
-
-    #        # Implement rip or opsf
-    #        pass
-    #    elif link["type"] == "inter-as":
-    #        # Implement bgp
-    #        pass
-            
-          #  if num_as == 1 :
-              #  rip_commandes("1000:0:0:1::1/64",interface_from,routeur1,routeur2)
-              #  print()
-              #  rip_commandes("1000:0:0:1::2/64",interface_to,routeur2,routeur1)
-              #  print()
-          #  if num_as == 2 :
-              #  ospf_commandes("1000:0:0:1::1/64",interface_from,routeur1,routeur2)
-              #  print()
-              #  ospf_commandes("1000:0:0:1::2/64",interface_to,routeur2,routeur1)
-              #  print()
     
-    ## g.recoverRouter(name="R1")
-
-    # print(tn.read_all().decode('ascii'))
-
-    # g.close()
-
-    # g = GnsProject(name="Test")
-    # g.createNew()
-    # g.recoverExisting()
-    # g.recoverRouter(name="R3")
-    # port = g.getRouterPort("R3")
-
-    # routerS = RouterSocket(port=port)
-    # routerS.run("configure terminal")
-    # routerS.run("interface g1/0")
-    # routerS.run("ipv6 enable")
-    # routerS.close()
+    # g.lab.arrange_nodes_circular()
