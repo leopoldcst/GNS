d586dbcd4a8ad8ff3ee3e1a056913d56549cd332
leopoldcst <leopoldconstant15@gmail.com>
2026-01-08 12:11:37 +0100

    ajout de ebgp dans commands.py

--- commands.py
+++ commands.py
@@ -28,8 +28,8 @@ def ripConfig(address, interface, name):
 
     process_name = "RIP_AS"
     conf = []
-    conf += baseRouterConfig(name)
-    conf += addressConfig(interface,address)
+    #conf += baseRouterConfig(name)
+    #conf += addressConfig(interface,address)
     conf += [
         "enable"
         "configure terminal"
@@ -48,8 +48,8 @@ def ripConfig(address, interface, name):
 def ospfConfig(address, interface, name, area_nb):
     conf = []
     process_id = int(name[1:])
-    conf += baseRouterConfig(name)
-    conf += addressConfig(interface,address)
+    #conf += baseRouterConfig(name)
+    #conf += addressConfig(interface,address)
     conf += [
         "enable",
         "configure terminal",
@@ -64,3 +64,58 @@ def ospfConfig(address, interface, name, area_nb):
     return conf
 
    #"redistribute connected" à activer si on veut partager tous les sous reseaux auxquels on appartient, ce qui n'est pas le cas tout le temps :)
+
+def ipv6_sans_masque(ipv6):
+    """Supprime le /64 si présent (obligatoire pour BGP neighbor)"""
+    return ipv6.split("/")[0]
+
+
+def ebgpConfig(address1, 
+               interface1,
+               name1,
+               as1, 
+               address2, 
+               interface2,
+               name2,
+               as2):
+    
+    neighbor_R1 = ipv6_sans_masque(address2)
+    neighbor_R2 = ipv6_sans_masque(address1)
+
+    commandes_1 = [
+        "configure terminal",
+        f"router bgp {as1}",
+        f" bgp router-id {as1}.{as1}.{as1}.{as1}",
+        " no bgp default ipv4-unicast",
+        f" neighbor {neighbor_R1} remote-as {as2}",
+        " address-family ipv6 unicast",
+        f"  neighbor {neighbor_R1} activate",
+        " exit-address-family",
+        "end"
+    ]
+
+    # =========================
+    # Commandes R2
+    # =========================
+    commandes_2 = [
+
+        "configure terminal",
+        f"router bgp {as2}",
+        f" bgp router-id {as2}.{as2}.{as2}.{as2}",
+        " no bgp default ipv4-unicast",
+        f" neighbor {neighbor_R2} remote-as {as1}",
+        " address-family ipv6 unicast",
+        f"  neighbor {neighbor_R2} activate",
+        " exit-address-family",
+        "end"
+    ]
+
+    return commandes_1, commandes_2
+
+
+#print(ebgpConfig("1001::1","g1/0","R1","1","1001:2","g1/0","R2","2"))
+
+
+    
+
+    
